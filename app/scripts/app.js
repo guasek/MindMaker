2200936b4d9b406316cf7b694bfc8d3eabb015cd
glasek <glasek@red-sky.pl>
2014-03-23 22:07:23 +0100

    first commit, project set up

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('mindMakerApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
