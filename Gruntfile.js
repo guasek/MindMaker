35c6ea3c2f39dc18974a31b87f0c49077193e7c5
Paweł Głasek <guasek@gmail.com>
2014-05-06 23:33:34 +0200

    Enough for now

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,4 @@
-// Generated on 2014-03-24 using generator-angular 0.7.1
 'use strict';
-
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
 
 module.exports = function (grunt) {
 
